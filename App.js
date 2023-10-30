d8edd1e96ead1dd893dbbd9516bf31363629a19a
vigneishwar <vikki1310@gmail.com>
2023-10-30 12:48:05 +1100

    added props

--- App.js
+++ App.js
@@ -39,10 +39,57 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = (props) => {
+  return (
+    <div className="res-card">
+      <img className ="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/83650e6ca1465c58063a70ad22be4f28" />
+      <h3>{props.resName}</h3>
+      <h4>{props.cusine}</h4>
+      <h4>4.8 Stars</h4>
+      <h4>45 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard resName = "Ambur Briyani" cusine = "Briyani South Indian"/>
+        <RestaurantCard resName = "KFC" cusine = "Burgers, Fast Food"/>
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
