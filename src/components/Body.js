068396020cb9971ab2c12300975fed56eb6fff75
vigneishwar <vikki1310@gmail.com>
2023-10-31 22:55:44 +1100

    added top rated restaurants with useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,24 @@ import restaurantLists from "../../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
+
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantLists);
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+        <button className="filter-btn" 
+        onClick={() => {
+          const filteredLists = listOfRestaurants.restaurants.filter(
+            (res) => res.info.avgRating > 4.3
+          );
+          setListOfRestaurants({restaurants: filteredLists});
+        }}
+        >
+          Top Rated Restaurants
+        </button>
+        </div>
         <div className="res-container">
-          {restaurantLists.restaurants.map((restaurant, index) => (
+          {listOfRestaurants.restaurants.map((restaurant, index) => (
             <RestaurantCard key = {index} resData={restaurant} />
           ))}
         </div>
