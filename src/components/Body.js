2f17b0e39868e02c8feef382a57b6ab87e9c650f
vigneishwar <vikki1310@gmail.com>
2023-11-16 16:06:07 +1100

    added api call using useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,9 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantLists from "../../utils/mockData";
 import { useState, useEffect } from "react";
 
 const Body = () => {
 
-  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantLists);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -15,29 +14,34 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0849557&lng=80.2101342&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-
+    console.log(json);
+    const restaurantData = json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants;
+    setListOfRestaurants(restaurantData);
   }; 
     return (
       <div className="body">
         <div className="filter">
         <button className="filter-btn" 
         onClick={() => {
-          const filteredLists = listOfRestaurants.restaurants.filter(
+          const filteredLists = listOfRestaurants.filter(
             (res) => res.info.avgRating > 4.5
           );
-          setListOfRestaurants({restaurants: filteredLists});
+          setListOfRestaurants(filteredLists);
         }}
         >
           Top Rated Restaurants
         </button>
         </div>
         <div className="res-container">
-          {listOfRestaurants.restaurants.map((restaurant, index) => (
+          {listOfRestaurants.map((restaurant, index) => (
             <RestaurantCard key = {index} resData={restaurant} />
           ))}
+          
         </div>
       </div>
     );
+    
   };
+  
 
   export default Body;
