e285b4542cbc558ef3540e2b5db54f4a357b5423
vigneishwar <vikki1310@gmail.com>
2023-11-21 12:15:16 +1100

    added some conditions to fetch data and handled errors

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,15 +11,47 @@ const Body = () => {
     fetchData();
   }, []);
 
+  // const fetchData = async () => {
+  //   const data = await fetch(
+  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0849557&lng=80.2101342&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  //   );
+  //   const json = await data.json();
+  //   console.log(json);
+  //   const restaurantData = json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+  //   setListOfRestaurants(restaurantData);
+  // }; 
+
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0849557&lng=80.2101342&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    console.log(json);
-    const restaurantData = json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-    setListOfRestaurants(restaurantData);
-  }; 
+    try {
+      const response = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0849557&lng=80.2101342&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      // checking for the response.
+      // returns error if there is http error
+      if(!response.ok) {
+        throw new Error(`HTTP ERROR! status :${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+      // looping through the entire json array to see for the restaurants
+      // setting the restaurantData to null and saving the response if the restaurants are found 
+      let restaurantData = null;
+      // looping through the json data or if it is null
+      for (const resCards of json?.data?.cards || []) {
+        if(resCards?.card?.card?.gridElements?.infoWithStyle?.restaurants) {
+          restaurantData = resCards.card.card.gridElements.infoWithStyle.restaurants;
+          break;
+        }
+      }
+      if(restaurantData) {
+        setListOfRestaurants(restaurantData);
+      } else{
+        console.log("Data not found");
+      }
+    } catch(error) {
+      console.error("Error in fetching data: ", error);
+    }
+  };
 
   // if (listOfRestaurants?.length === 0) {
   //   return <Shimmer />;
@@ -39,7 +71,8 @@ const Body = () => {
         </button>
         </div>
         <div className="res-container">
-          {listOfRestaurants.map((restaurant, index) => (
+          {console.log(listOfRestaurants)}
+          {listOfRestaurants?.map((restaurant, index) => (
             <RestaurantCard key = {index} resData={restaurant} />
           ))}
           
